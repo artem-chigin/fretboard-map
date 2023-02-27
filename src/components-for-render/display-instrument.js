255db07fad04cd1ecde553f93848d13672bfeb18
artem-chigin <chiartftc@gmail.com>
2023-02-27 14:52:38 +0400

    Colored notes on fretboard

--- src/components-for-render/display-instrument.js
+++ src/components-for-render/display-instrument.js
@@ -21,9 +21,34 @@ function FretNumbers({fretNumbers}) {
 
 
 function Note({note, noteIndex, stringIndex}) {
+
+    const noteColors = {
+        "C": "#BA0000",
+        "C#": "6900BA",
+        "Db": "6900BA",
+        "D": "#BABA00",
+        "D#": "#863365",
+        "Eb": "#863365",
+        "E": "#8EB1BA",
+        "F": "#740001",
+        "F#": "#5D65B9",
+        "Gb": "#5D65B9",
+        "G": "#BA5D00",
+        "G#": "#8955B8",
+        "Ab": "#8955B8",
+        "A": "#259525",
+        "A#": "#7B4B5B",
+        "Bb": "#7B4B5B",
+        "B": "#6893BA"
+    }
+
+    const noteColor = noteColors[note.noteName]
+
     return (
         <td id={"string" + stringIndex + "note" + noteIndex} className="noteContainer">
-            <div className='note' visibility={note.visibility}>{note.noteName + note.octave}</div>
+            <div className='note' 
+                visibility={note.visibility}
+                style={{backgroundColor: noteColor}}>{note.noteName + note.octave}</div>
         </td>
     )
 }
