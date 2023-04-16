c88186387383fb343415afe174d3970e8e6aec14
artem-chigin <chiartftc@gmail.com>
2023-04-16 16:56:49 +0400

    Created circle of fifths template and scc

--- src/components-for-render/circle-of-fifths/display-circle.js
+++ src/components-for-render/circle-of-fifths/display-circle.js
@@ -1,43 +1,58 @@
 import MUSIC_CONST from "../../instrument-scale-calculations/musical-constants";
 import MusicalInstrument from "../../instrument-scale-calculations/musical-instrument";
 
+// const elementStyle = {border: "solid", width: "100px", height: "100px", boxSizing: "content-box"};
 
-function CircleElement({majorScaleName, minorScaleName}) {
 
-    // const instrumentSettings = {instrumentName: "default", fretsNumber: 24, tuning: ["C1"]};
-    // const instrument = new MusicalInstrument(instrumentSettings);
+function CircleElement({scale, idName}) {
 
     return (
-        <div className="circle-element" style={{border: "solid", width: "185px", boxSizing: "content-box"}}>
-            <div> {majorScaleName} </div>
-            <div> {minorScaleName + "m"} </div>
+        <div className={"circle-element"} id={idName}>
+            <div id={"outer-" + idName}> {scale[0]} </div>
+            <div id={"inner-" + idName}> {scale[5] + "m"} </div>
         </div>
     )
 }
 
 function DisplayCircle() {
-    const scales = [];
+    let scales = {};
     let sharpFlag = true;
 
     const instrument = new MusicalInstrument({instrumentName: "", fretsNumber: 12, tuning: ["C1"]})
 
     let noteIndex = 0;
-    while (scales.length < 12) {
-        if (scales.length > 6) {
+    while (Object.keys(scales).length < 12) {
+        if (Object.keys(scales).length > 6) {
             sharpFlag = false;
         }
-        scales.push([instrument.generateSeqence([MUSIC_CONST.chromaticScale[noteIndex], 1], MUSIC_CONST.scales.major, sharpFlag)]);
+        let rootNote = MUSIC_CONST.chromaticScale[noteIndex];
+        scales[rootNote] = instrument.generateSeqence([rootNote, 1], MUSIC_CONST.scales.major, sharpFlag);
         noteIndex = (noteIndex + MUSIC_CONST.intervals.p5[0]) % MUSIC_CONST.notesInChromaticScale;
     }
 
-    const displayScales = scales.map((scale, index) =>
+    function renameNote(noteName) {
+        if (noteName.includes("#")) {
+            return noteName.replace("#", "-sharp");
+        }
+        if (noteName.includes("b")) {
+            return noteName.replace("b", "-flat");
+        }
+        return noteName
+    }
+
+    const displayScales = Object.keys(scales).map((note, index) =>
         <CircleElement 
-        majorScaleName={scale[0][0]} minorScaleName={scale[0][5]} 
-        key={"scale" + index}/>
+            scale={scales[note]} 
+            key={"scale" + index}
+            idName={"scale-" + renameNote(scales[note][0])}/>
         )
-    return <div 
-            style={{display: "flex", width: "600px", flexWrap: "wrap"}}
-            >{displayScales}</div>
+
+        // console.log(displayScales[0]);
+    return <div className="circle-of-fifths-container">
+                {displayScales}
+                <div className="inner-circle"></div>
+                
+            </div>
 }
 
 export default DisplayCircle;
