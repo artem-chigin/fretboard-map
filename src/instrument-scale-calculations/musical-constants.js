0f4507a160fd389bcf41dcc0d8b2ab242c48bf0e
artem-chigin <chiartftc@gmail.com>
2023-04-17 13:23:15 +0400

    Maked chord schemme generation from intervals (not from raw magic numbers)

--- src/instrument-scale-calculations/musical-constants.js
+++ src/instrument-scale-calculations/musical-constants.js
@@ -1,39 +1,66 @@
-const MUSIC_CONST = {
-    chromaticScale: ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
-    chromaticScaleFlat: ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"],
-    notesInChromaticScale: 12,
-    intervals: {
-        p1: [0],
-        m2: [1],
-        M2: [2],
-        m3: [3], 
-        M3: [4],
-        p4: [5],
-        tritone: [6],
-        p5: [7],
-        m6: [8],
-        M6: [9],
-        m7: [10],
-        M7: [11],
-        p8: [12]
-    },
-    chords: {
-            maj: [4, 3],
-            m: [3, 4],
-            aug: [4, 4],
-            dim: [3, 3],
-            m6: [3, 4, 2],
-            maj7: [4, 3, 4],
-            m7: [3, 4, 3],
-            dominant7: [4, 3, 3],
-            sus2: [2, 5],
-            sus4: [5, 2],
+const chromaticScaleSharp =  ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const chromaticScaleFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+const INTERVALS = {
+    p1: [0],
+    m2: [1],
+    M2: [2],
+    m3: [3], 
+    M3: [4],
+    p4: [5],
+    tritone: [6],
+    p5: [7],
+    m6: [8],
+    M6: [9],
+    m7: [10],
+    M7: [11],
+    p8: [12]
+};
 
-    },
-    scales: {
-        major: [2, 2, 1, 2, 2, 2, 1],
-        minor: [2, 1, 2, 2, 1, 2, 2],
-    },
+function createChordScheme(chordBase, newInterval) {
+    return chordBase.concat(newInterval)
+};
+
+const CHORDS_TRIAD = {
+    maj: createChordScheme(INTERVALS.M3, INTERVALS.m3),
+    m: createChordScheme(INTERVALS.m3, INTERVALS.M3),
+    aug: createChordScheme(INTERVALS.M3, INTERVALS.M3),
+    dim: createChordScheme(INTERVALS.m3, INTERVALS.m3),
+}
+
+const CHORDS_SUS = {
+    sus2: createChordScheme(INTERVALS.M2, INTERVALS.p4),
+    sus4: createChordScheme(INTERVALS.p4, INTERVALS.M2),
+}
+
+const CHORDS =  {
+    maj: CHORDS_TRIAD.maj,
+    m: CHORDS_TRIAD.m,
+    aug: CHORDS_TRIAD.aug,
+    dim: CHORDS_TRIAD.dim,
+
+    m6: createChordScheme(CHORDS_TRIAD.m, INTERVALS.M2),
+
+
+    maj7: createChordScheme(CHORDS_TRIAD.maj, INTERVALS.M3),
+    m7: createChordScheme(CHORDS_TRIAD.m, INTERVALS.m3),
+    dominant7: createChordScheme(CHORDS_TRIAD.maj, INTERVALS.m3),
+    
+    sus2: CHORDS_SUS.sus2,
+    sus4: CHORDS_SUS.sus4
+};
+
+const SCALES = {
+    major: [2, 2, 1, 2, 2, 2, 1],
+    minor: [2, 1, 2, 2, 1, 2, 2],
+};
+
+const MUSIC_CONST = {
+    chromaticScale: chromaticScaleSharp,
+    chromaticScaleFlat: chromaticScaleFlat,
+    notesInChromaticScale: chromaticScaleSharp.length,
+    intervals: INTERVALS,
+    chords: CHORDS,
+    scales: SCALES,
     // note: {single_note: [0]}
 };
 
