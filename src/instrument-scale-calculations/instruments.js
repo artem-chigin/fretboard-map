73db0505f68f5360cf72287926e53d0caaa1d337
artem-chigin <chiartftc@gmail.com>
2024-03-27 20:46:21 +0400

    corrected typo

--- src/instrument-scale-calculations/instruments.js
+++ src/instrument-scale-calculations/instruments.js
@@ -1,9 +1,24 @@
 const instruments = {
-    "Bariton Ukulele": {instrumentName: "Bariton Ukulele", fretsNumber: 19, tuning: ["D3", "G3", "B3", "E4"]},
-    "Ukulele": {instrumentName: "Ukulele", fretsNumber: 19, tuning: ["G4", "C4", "E4", "A4"]},
-    "Bass Guitar": {instrumentName: "Bass Guitar", fretsNumber: 24, tuning: ["E1", "A1", "D2", "G2"]},
-    "Guitar": {instrumentName: "Guitar", fretsNumber: 24, tuning: ["E2", "A2", "D3", "G3", "B3", "E4"]}
-
+  "Baritone Ukulele": {
+    instrumentName: "Baritone Ukulele",
+    fretsNumber: 19,
+    tuning: ["D3", "G3", "B3", "E4"],
+  },
+  Ukulele: {
+    instrumentName: "Ukulele",
+    fretsNumber: 19,
+    tuning: ["G4", "C4", "E4", "A4"],
+  },
+  "Bass Guitar": {
+    instrumentName: "Bass Guitar",
+    fretsNumber: 24,
+    tuning: ["E1", "A1", "D2", "G2"],
+  },
+  Guitar: {
+    instrumentName: "Guitar",
+    fretsNumber: 24,
+    tuning: ["E2", "A2", "D3", "G3", "B3", "E4"],
+  },
 };
 
 export default instruments;
