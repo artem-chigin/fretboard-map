5e4d19504534a7970fac9acddbdd91878746bd6b
artem-chigin <chiartftc@gmail.com>
2024-09-04 10:24:23 +0400

    minor css changes

--- src/components-for-render/fretboard-map/display-sequence.js
+++ src/components-for-render/fretboard-map/display-sequence.js
@@ -1,7 +1,5 @@
-function DisplaySequence({currentSequence}) {
-    return (
-<label>{currentSequence.join(" - ")}</label>        
-    )
+function DisplaySequence({ currentSequence }) {
+  return <label>{currentSequence.join(" - ")}</label>;
 }
 
 export default DisplaySequence;
